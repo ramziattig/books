509fc7d35f1deb76a19eeaaba6e5c88c588d6fb3
ramzi attig <ramziattig@gmail.com>
2020-07-08 20:19:56 +0100

    change data models/views/controllers/assets

--- backoffice/models/category.php
+++ backoffice/models/category.php
@@ -1,5 +1,5 @@
 <?php
-class Author{
+class Category{
 
     public $id;
     public $name;
@@ -8,7 +8,7 @@ class Author{
     public static function getAll(){
 
         $base = connect_to_db();
-        $requette = "SELECT * From authors ;";
+        $requette = "SELECT * From categories ;";
         $data = $base->query($requette);
 
         return $data;
@@ -17,10 +17,10 @@ class Author{
     public function insert(){
         $base = connect_to_db();
 
-        $requette = "INSERT INTO authors VALUES(null,'$this->name');";
+        $requette = "INSERT INTO categories VALUES(null,'$this->name');";
         $rowInserted = $base->exec($requette);
         if ($rowInserted == 1) {
-            header('location:../views/authors/allAuthors.php');      
+            header('location:../views/categories/allCategories.php');
         }else {
             echo "SQL Error !";
         }
@@ -29,10 +29,10 @@ class Author{
     public function edit(){
         $base = connect_to_db();
 
-        $requette = "update authors set name ='$this->name' where id=$this->id";
+        $requette = "update categories set name ='$this->name' where id=$this->id";
         $rowUpdated = $base->exec($requette);
         if ($rowUpdated == 1) {
-            header('location:../views/authors/allAuthors.php');
+            header('location:../views/categories/allCategories.php');
         }else {
             echo "SQL Error !";
         }
@@ -41,10 +41,10 @@ class Author{
     public function delete(){
         $base = connect_to_db();
 
-        $requette = "delete from  authors where id=$this->id;";
+        $requette = "delete from  categories where id=$this->id;";
         $rowDeleted = $base->exec($requette);
         if ($rowDeleted == 1) {
-            header('location:../views/authors/allAuthors.php');
+            header('location:../views/categories/allCategories.php');
         }else {
             echo "SQL Error !";
         }
